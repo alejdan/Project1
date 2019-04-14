e6440915bb50140f343f307e926dafe2e8bf3342
alejdan <alejdan@gmail.com>
2019-04-14 14:02:26 -0500

    basic HTML and CSS skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBUnDtEPZ7ByqCOhoro4Cgl22IbE6w699A",
+    authDomain: "project1-39be4.firebaseapp.com",
+    databaseURL: "https://project1-39be4.firebaseio.com",
+    projectId: "project1-39be4",
+    storageBucket: "project1-39be4.appspot.com",
+    messagingSenderId: "145278155405"
+};
+firebase.initializeApp(config);
+
